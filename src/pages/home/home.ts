4409ed9699c68a5a0113ba8502a5a8d5a8b93915
Irving Didier <bider88@gmail.com>
2018-08-27 00:33:00 -0500

    Creando el logout y finalizando proyecto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,9 @@ import { NavController } from 'ionic-angular';
 import { UserProvider } from '../../providers/user/user';
 import { User } from '../../models/user.model';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import { LoginPage } from '../login/login';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -13,10 +16,24 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    private _userProvider: UserProvider
+    private _userProvider: UserProvider,
+    private afAuth: AngularFireAuth
   ) {
     console.log(this._userProvider.user);
     this.user = this._userProvider.user;
+
+    // Verificamos el usuario está logueado si es null es que no se encuentra logueado
+    this.afAuth.authState.subscribe( user => {
+      console.log('AfAuth');
+      console.log( JSON.stringify(user) );
+    });
+  }
+
+  logout() {
+    this.afAuth.auth.signOut().then( res => {
+      this._userProvider.user = {};
+      this.navCtrl.setRoot(LoginPage);
+    });
   }
 
 }
